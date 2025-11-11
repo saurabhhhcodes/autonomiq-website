ffb3b67d8612273da18de9cc2c2842fbdffa5ada
8packcoder <[saurabhbajpai03@outlook.com]>
2025-11-11 12:47:44 +0530

    Update Firebase config with real credentials

--- frontend/js/firebase-config.js
+++ frontend/js/firebase-config.js
@@ -1,9 +1,10 @@
 // Firebase Configuration for AxonFlow
 const firebaseConfig = {
-    apiKey: "AIzaSyDqZ8vK9xJ3mN2pL5wR7tY8uI6oP4aS1dF",
+    apiKey: "AIzaSyD10WdBkvyLYTSp30wfD5ACJ-pu24LjWjU",
     authDomain: "axonflow-auth.firebaseapp.com",
     projectId: "axonflow-auth",
-    storageBucket: "axonflow-auth.appspot.com",
-    messagingSenderId: "346992451501",
-    appId: "1:346992451501:web:a1b2c3d4e5f6g7h8i9j0"
+    storageBucket: "axonflow-auth.firebasestorage.app",
+    messagingSenderId: "277021265161",
+    appId: "1:277021265161:web:dc351877acb855fa3c97d8",
+    measurementId: "G-Q99RG63QJB"
 };
